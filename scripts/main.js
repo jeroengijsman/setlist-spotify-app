1f2af6cf2e8cc11f892e1831e20b9990488bd636
Jeroen Gijsman <jeroen@q42.nl>
2014-02-13 23:50:59 +0100

    fixed get calls - added topartist images - first setup for search suggestions

--- scripts/main.js
+++ scripts/main.js
@@ -3,22 +3,74 @@ var Setlist = {
     Models: null,
     Search: null,
     List: null,
+    Toplist: null,
     playlistUri: null,
     playlistView: null,
+    topArtistImageList: [],
 
-    init: function(Models, Search, List) {
+    init: function(Models, Search, List, Toplist) {
         Setlist.Models = Models;
         Setlist.Search = Search;
         Setlist.List = List;
+        Setlist.Toplist = Toplist;
 
-        $("#searchButton").live("click", function(event) {
-            event.preventDefault();
+        var list = Setlist.Toplist.forCurrentUser();
+
+        list.artists.snapshot().done(function(artists) {
+            for (var i = 0; i < artists.length; i++) {
+                Setlist.Models.Artist.fromURI(artists.get(i).uri).load('image').done(function(artist) {
+                    Setlist.topArtistImageList.push(artist.imageForSize(420));
+                });
+            }
+            $('#header img').attr("src", Setlist.topArtistImageList[0]);
+        });
+
+        $('#searchSetlist').submit(function(event) {
             Setlist.search();
+            event.preventDefault();
+        });
+
+        var searchTimer;
+        $('#searchField').keyup(function() {
+            var query = $(this).val();
+            clearTimeout(searchTimer);
+            if(query.length > 3) {
+                searchTimer = setTimeout(function() {
+                    Setlist.searchSuggestions(query);
+                }, 250);
+            }
+            else {
+                $('#searchSuggestion').hide();
+            }
         });
 
         Setlist.Models.Playlist.createTemporary(Date.now()).done(function(playlist) {    
             playlistUri = playlist.uri;
         });
+
+        Setlist.changeWallpaper(1);
+    },
+
+    searchSuggestions: function(query) {
+        $.get('http://search.musicbrainz.org/ws/3/artist/?query='+ query +'&fmt=json', function(data) {
+            $('#searchSuggestion').show();
+            $('#searchSuggestion').html('<li>' + data['artist-list'].artist[0].name + '</li>')
+            console.log(data['artist-list'].artist[0]);
+        });
+    },
+
+    changeWallpaper: function(nextWallpaper) {
+        setTimeout(function()   { 
+            $('#header img').fadeOut(function() {
+                $('#header img').attr("src", Setlist.topArtistImageList[nextWallpaper]);
+                $('#header img').fadeIn();
+            });
+            nextWallpaper++;
+            if(nextWallpaper > Setlist.topArtistImageList.length)
+                nextWallpaper = 0;
+
+            Setlist.changeWallpaper(nextWallpaper);
+         }, 6000);
     },
 
     search: function() {
@@ -110,8 +162,9 @@ var Setlist = {
 require([
   '$api/models',        
   '$api/search#Search',
-  '$views/list#List'
-], function(Models, Search, List) {
+  '$views/list#List',
+  '$api/toplists#Toplist'
+], function(Models, Search, List, Toplist) {
 
-    Setlist.init(Models, Search, List);
+    Setlist.init(Models, Search, List, Toplist);
 });
